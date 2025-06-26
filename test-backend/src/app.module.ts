e05f5d7b2b94848e6032c967d63ae36e589459f5
HO NGUYEN Nhat-Quan <hnnhat67@gmail.com>
2025-06-26 18:47:52 +0200

    🔧 F01 > Ajout de la configuration de GraphQL et TypeORM dans le module principal de l'application. Intégration de MySQL avec les paramètres de connexion appropriés et activation de la synchronisation des entités.

--- test-backend/src/app.module.ts
+++ test-backend/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({ autoSchemaFile: true }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'NestJs_2025!',
+      database: 'test_crud',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
