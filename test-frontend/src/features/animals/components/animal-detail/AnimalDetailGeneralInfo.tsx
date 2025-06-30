0daa0db6623fb243c81115f5fd4786dc83407cc1
HO NGUYEN Nhat-Quan <hnnhat67@gmail.com>
2025-07-01 01:27:15 +0200

    🔄 F04 > Traduction du libellé "Espèce" en anglais par "Species" pour une meilleure accessibilité internationale dans les détails des animaux.

--- test-frontend/src/features/animals/components/animal-detail/AnimalDetailGeneralInfo.tsx
+++ test-frontend/src/features/animals/components/animal-detail/AnimalDetailGeneralInfo.tsx
@@ -16,7 +16,7 @@ export default function AnimalDetailGeneralInfo({
       </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
         <div className="p-4 bg-accent/30 rounded-lg">
-          <p className="font-medium text-sm text-muted-foreground">Espèce</p>
+          <p className="font-medium text-sm text-muted-foreground">Species</p>
           <p className="font-semibold">{animal.species}</p>
         </div>
         <div className="p-4 bg-accent/30 rounded-lg">
